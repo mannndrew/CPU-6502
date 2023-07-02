5040354d8969b0e16b2a6fabf6da51b1eb855f91
Andrew Howard <mannndrew@gmail.com>
2023-07-01 20:37:19 -0500

    Update simulator.py

--- Simulator/simulator.py
+++ Simulator/simulator.py
@@ -1,6 +1,13 @@
 import sys
+import time
+
+# Print welcome message
+print(f"Welcome to the W65C02S simulator!\n")
 
 ########################################################### File IO ###########################################################
+
+# Print loading message
+print("Loading in hexdump...")
 
 # Check if the user has provided a file name
 if len(sys.argv) != 2:
@@ -33,6 +40,80 @@ for line in file:
 # Close file
 file.close()
 
+# Print loading message
+print(f"Hexdump loaded successfully!\n")
+
 
 ########################################################### Simulator ###########################################################
-print(memory)
+
+def inc_pc(reg_pch, reg_pcl):
+    if reg_pch == 0xFF and reg_pcl == 0xFF:
+        reg_pch = 0x00
+        reg_pcl = 0x00
+
+    elif reg_pcl == 0xFF:
+        reg_pch += 0x01
+        reg_pcl = 0x00
+    
+    else:
+        reg_pcl += 0x01
+
+    return reg_pch, reg_pcl
+
+# Default vectors
+nonmaskable_interupt_vector_high = 0xFFFA
+nonmaskable_interupt_vector_low = 0xFFFB
+reset_vector_high = 0xFFFC
+reset_vector_low = 0xFFFD
+interupt_vector_high = 0xFFFE
+interupt_vector_low = 0xFFFF
+    
+# Stack (256 bytes)
+stack = [] * 256
+
+# Registers
+reg_x = 0x00
+reg_y = 0x00
+reg_sp = 0xFF
+reg_a = 0x00
+reg_pch  = 0x00
+reg_pcl = 0x00
+reg_flags = 0x00
+reg_inst = 0x00
+
+# Fetch instruction: 1 cycle
+time.sleep(1)
+reg_inst = memory[0]
+reg_pch, reg_pcl = inc_pc(reg_pch, reg_pcl)
+
+while reg_inst != None:
+    match reg_inst:
+        case "00":
+            # BRK: 6 cycles
+
+            # Push PCH to stack
+            time.sleep(1)
+            stack[reg_sp] = reg_pch
+            reg_sp -= 0x01
+
+            # Push PCL to stack
+            time.sleep(1)
+            stack[reg_sp] = reg_pcl
+            reg_sp -= 0x01
+
+            # Push flags to stack & set break/interupt flag
+            time.sleep(1)
+            stack[reg_sp] = reg_flags
+            reg_sp -= 0x01
+            reg_flags |= 0b00010100
+
+            # Load interrupt vector low byte
+            time.sleep(1)
+            reg_pcl = memory[nonmaskable_interupt_vector_low]
+
+            # Load interrupt vector high byte
+            time.sleep(1)
+            reg_pch = memory[nonmaskable_interupt_vector_high]
+
+
+# print(memory)
